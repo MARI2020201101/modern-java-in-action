f70ecbb0d0e9a8732064e1009d0f4d294c207ad9
MARI <mari20201101@gmail.com>
2021-12-27 17:48:43 +0900

    Practice chain of responsibility with lambda

--- src/ch09/ChainOfResponsibilityTest.java
+++ src/ch09/ChainOfResponsibilityTest.java
@@ -1,5 +1,8 @@
 package ch09;
 
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
 public class ChainOfResponsibilityTest {
     public static void main(String[] args) {
         String string = "modern java in action";
@@ -10,5 +13,13 @@ public class ChainOfResponsibilityTest {
 
         String processedString = p2.handle(string);
         System.out.println(processedString);
+
+        System.out.println("----------------");
+        UnaryOperator<String> u1 = s ->"######\n" + s + "\n######";
+        UnaryOperator<String> u2 = String::toUpperCase;
+        Function<String,String> f = u1.andThen(u2);
+        
+        String processedByLambda = f.apply(string);
+        System.out.println(processedByLambda);
     }
 }
